d1d58d230dde859d4354966b76d05014588d2c1d
El2santos <61806601+El2santos@users.noreply.github.com>
2020-03-04 15:47:16 -0600

    Parcial

---
+++ mokup.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <?php
+    echo "<br>;";
+    echo "<h1> Asistencia</h1>";
+
+echo "<table border=2>";
+    echo "<tr>";
+    echo "<br>";
+    echo "<th>Guardar</th>";
+    echo "<th>Nuevo</th>";
+    echo "<th>Eliminar</th>";
+    echo "<th>Modificar</th>";
+    echo "</tr>";
+    echo "</table>";
+
+    echo "<table border=2>";
+    echo "<tr>";
+    echo "<br>";
+    echo "<th>Alumno</th>";
+    echo "<th>grado</th>";
+    echo "<th>Maestro</th>";
+    echo "<th>Asistencia</th>";
+    echo "</tr>";
+
+    echo "<tr>";
+    
+    echo "<td>Axel Lopez</td>";
+    echo "<td>1ro Primaria</td>";
+    echo "<td>Carlos</td>";
+    echo "<td>Si vino</td>";
+    
+    echo "</tr>";
+    echo "</table>";
+    ?>
+    </center>
+    
+
+</body>
+</html>
